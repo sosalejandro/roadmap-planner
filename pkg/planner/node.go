16c5ee3a1496a533227b2f6b3eb6adf681d36529
Alejandro <alejandrososadev@gmail.com>
2022-12-11 21:29:58 -0400

    Adds node base class

---
+++ pkg/planner/node.go
@@ -0,0 +1,19 @@
+package planner
+
+import "fmt"
+
+// Node is a representation of an entity and all its possible relationships
+type Node struct {
+	name string
+	//Has     []*Node
+	//Creates []*Node
+	//Blocks  []*Node
+}
+
+func (n *Node) String() string {
+	return fmt.Sprintf("%s", n.name)
+}
+
+func NewNode(name string) *Node {
+	return &Node{name: name}
+}
---
+++ pkg/planner/node_test.go
@@ -0,0 +1,60 @@
+package planner
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNewNode(t *testing.T) {
+	type args struct {
+		name string
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want *Node
+	}{
+		// TODO: Add test cases.
+		{
+			name: "Creates new node called Patricia",
+			args: args{name: "Patricia"},
+			want: &Node{name: "Patricia"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewNode(tt.args.name); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewNode() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNode_String(t *testing.T) {
+	type fields struct {
+		name string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		// TODO: Add test cases.
+		{
+			name:   "Node is called Patricia",
+			fields: fields{name: "Patricia"},
+			want:   "Patricia",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			n := &Node{
+				name: tt.fields.name,
+			}
+			if got := n.String(); got != tt.want {
+				t.Errorf("String() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
