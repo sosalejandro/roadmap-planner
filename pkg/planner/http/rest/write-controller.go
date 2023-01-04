4e759ac6b4025e186aeedcd1083aa5697ca975f8
Alejandro <alejandrososadev@gmail.com>
2023-01-04 10:58:27 -0400

    Add controller missing implementation

--- pkg/planner/http/rest/write-controller.go
+++ pkg/planner/http/rest/write-controller.go
@@ -3,9 +3,11 @@ package rest
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/gorilla/mux"
 	"net/http"
+	"strconv"
 	"strings"
+
+	"github.com/gorilla/mux"
 )
 
 type WriteController struct {
@@ -18,12 +20,17 @@ func NewWriteController(list Service) *WriteController {
 
 func (c *WriteController) AddToList(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
-	idea := params["idea"]
+	id, idea := params["id"], params["idea"]
 
 	// validate idea is not empty or whitespace
 	if len(strings.TrimSpace(idea)) == 0 {
-		err := fmt.Sprintf("idea param is empty")
-		http.Error(w, err, http.StatusBadRequest)
+		err := fmt.Errorf("idea param is empty")
+		throwBadRequest(w, err)
+		return
+	}
+
+	if err := c.list.AddIdea(id, idea); err != nil {
+		throwBadRequest(w, err)
 		return
 	}
 
@@ -33,13 +40,78 @@ func (c *WriteController) AddToList(w http.ResponseWriter, r *http.Request) {
 }
 
 func (c *WriteController) InsertAtList(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, subId, idea := params["id"], params["subId"], params["idea"]
 
+	subIdInt, err := strconv.Atoi(subId)
+	if err != nil || subIdInt < 0 {
+		throwBadRequest(w, err)
+		return
+	}
+
+	// validate idea is not empty or whitespace
+	if len(strings.TrimSpace(idea)) == 0 {
+		err := fmt.Errorf("idea param is empty")
+		throwBadRequest(w, err)
+		return
+	}
+
+	err = c.list.InsertAtList(id, subIdInt, idea)
+	if err != nil {
+		throwBadRequest(w, err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	msg := fmt.Sprintf("New idea has been added at %s list.", id)
+	json.NewEncoder(w).Encode(msg)
 }
 
 func (c *WriteController) PromoteToSublist(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, ideaId := params["id"], params["idea"]
 
+	ideaIdInt, err := strconv.Atoi(ideaId)
+	if err != nil || ideaIdInt < 0 {
+		throwBadRequest(w, err)
+		return
+	}
+
+	name, err := c.list.PromoteToSublist(id, ideaIdInt)
+	if err != nil {
+		throwBadRequest(w, err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	msg := fmt.Sprintf("Idea %s has been promoted to sublist.", name)
+	json.NewEncoder(w).Encode(msg)
 }
 
 func (c *WriteController) CreateList(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	name := params["name"]
 
+	// validate name is not empty or whitespace
+	if len(strings.TrimSpace(name)) == 0 {
+		err := fmt.Errorf("idea param is empty")
+		throwBadRequest(w, err)
+		return
+	}
+
+	name, err := c.list.CreateList(name)
+	if err != nil {
+		throwBadRequest(w, err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	msg := fmt.Sprintf("List [%s] has been created.", name)
+	json.NewEncoder(w).Encode(msg)
+}
+
+func throwBadRequest(w http.ResponseWriter, err error) {
+	http.Error(w, err.Error(), http.StatusBadRequest)
 }
